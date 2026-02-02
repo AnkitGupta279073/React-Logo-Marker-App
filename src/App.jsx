da60bf0626ec555b6604f44a6e4cce1a48166132
Ankit Gupta <ankit.gupta@ankitguptaass-811-fhd.successivetech.com>
2026-02-02 23:09:04 +0530

    setup library and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 
+import { Button } from "./components/ui/button";
 function App() {
 
   return (
     <>
-      <h1>Logo Marker App</h1>
+      <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Button>Click me</Button>
+      
     </>
   )
 }
